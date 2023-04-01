b0c67538a36efd2f0db32ee77f0e562ae6e611a4
s22288 <s22288@pjwstk.edu.pl>
2023-04-01 16:09:55 +0200

    repair

--- 00-prework-js/script.js
+++ 00-prework-js/script.js
@@ -15,17 +15,7 @@ const products = [
         name: 'baton',
         price: 1.99
     }
-]
-
-suma = products.reduce(function (a, b) {
-    a.price + b.price;
-}, 0)
-
-console.log(suma)
-
-liczba = products.length
-
-//   srednia
-
-avg = suma / liczba
-document.write(avg)
+];
+let len = products.length;
+let avg = (products.reduce((a, b) => a += b.price, 0)) / len
+console.log(avg)
